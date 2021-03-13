04f75cfec6894e7b479a7cb4272fe0b1a631788c
Hasib787 <mr.hasib45@gmail.com>
2021-03-13 20:50:42 +0600

    make sign in with google

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  const handleSignIn = () => {
+    firebase.auth()
+      .signInWithPopup(provider)
+      .then(res=> {
+        const {displayName, email, photoURL }= res.user;
+        console.log(displayName, email, photoURL);
+      })
+}
+
+return (
+  <div className="App">
+    <h1>Firebase Auth</h1>
+    <button onClick={handleSignIn}>Sign in</button>
+  </div>
+);
 }
 
 export default App;
