8c37169919459ff2593276e99bd93ad44d16a151
Hasib787 <mr.hasib45@gmail.com>
2021-03-13 17:47:31 +0600

    maked a firebase config file

---
+++ src/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDyt4rA8A5gWm7WBFcDsjRil-yed5uQuOw",
+    authDomain: "ema-jhon-hshop.firebaseapp.com",
+    projectId: "ema-jhon-hshop",
+    storageBucket: "ema-jhon-hshop.appspot.com",
+    messagingSenderId: "952322486873",
+    appId: "1:952322486873:web:64c18516e3fc7cb5fdf748"
+  };
+  export default firebaseConfig;
